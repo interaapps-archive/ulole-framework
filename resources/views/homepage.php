7bef3db5305e9a53dcfaaa234f7379634eba3399
JulianFun123 <julianfun123@yahoo.com>
2019-08-31 17:30:26 +0200

    Fixed SQL save

--- resources/views/homepage.php
+++ resources/views/homepage.php
@@ -1,17 +1,3 @@
-<?php use databases\TestTable;
-
-tmpl('header'); ?>
-<?php
-
-    $test = (new TestTable);
-    $test->password = "Hello xDD";
-    echo $test->save();
-
-
-    foreach ((new TestTable)->select("*")->get() as $a ) {
-        echo $a["password"]."<br>";
-    }
-
-  #?>
+<?php tmpl('header'); ?>
 WELCOME
 <?php tmpl('footer'); ?>
